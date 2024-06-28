e5b4b024c6b37a8be54f11c1af8e3c1f43dacb06
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2024-06-28 19:50:01 +0100

    Added Cloudinary

--- immigration/settings.py
+++ immigration/settings.py
@@ -116,11 +116,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+
+]
+
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -250,8 +260,6 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'zZrsexxvBVryHpiyJ6DG2tZrl5Y',
 }
 
-# Media files
-MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
