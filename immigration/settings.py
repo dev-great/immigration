085a2954a29f63855372382e024a833615cbb7e4
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2024-06-28 19:55:52 +0100

    Added Cloudinary

--- immigration/settings.py
+++ immigration/settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary_storage
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
